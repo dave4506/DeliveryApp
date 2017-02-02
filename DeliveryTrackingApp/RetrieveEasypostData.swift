f1862c3a385447aa71fada2e94e7ef25280222c6
Pranav Madanahalli <drumin99@gmail.com>
2017-02-01 17:27:09 -0800

    Created first model class. In addition, we are going to be user Unit tests in this project. So i created a unit test class for the recently created model.

---
+++ DeliveryTrackingApp/RetrieveEasypostData.swift
@@ -0,0 +1,13 @@
+//
+//  RetrieveEasypostData.swift
+//  DeliveryTrackingApp
+//
+//  Created by Pranav Madanahalli on 2/1/17.
+//  Copyright © 2017 Download Horizons. All rights reserved.
+//
+
+import Foundation
+
+class RetrieveEasypostData {
+
+}
