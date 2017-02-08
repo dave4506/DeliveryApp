1f533d8c3c328085e0858ab26e4f8d1aa070d6b8
David Sun <ffstudios1@gmail.com>
2017-02-07 20:30:18 -0800

    Commented out logic. Note: (Don't push testing files to develop)

--- DeliveryTrackingApp/ViewController.swift
+++ DeliveryTrackingApp/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let retrieve = RetrieveEasypostData()
+    //let retrieve = RetrieveEasypostData()
     
-    let apiKEy = InitializeEasypostAPIKey()
+    //let apiKEy = InitializeEasypostAPIKey()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        apiKEy.setAndCheckAPIKeys()
+        //apiKEy.setAndCheckAPIKeys()
         
-        retrieve.createTrackerObjectWithTrackingCode(trackingCode: "EZ1000000001", carrierCall: "USPS")
+        //retrieve.createTrackerObjectWithTrackingCode(trackingCode: "EZ1000000001", carrierCall: "USPS")
         //retrieve.updateTrackingObjectWithTrackingID(trackID: "trk_c8e0edb5bb284caa934a0d3db23a148z")
                 
     }
