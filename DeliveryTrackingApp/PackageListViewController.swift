886284026a76cacf22c2b8c16047e14a69b4fa1c
David Sun <ffstudios1@gmail.com>
2017-03-03 23:04:22 -0800

    Added VC files

---
+++ DeliveryTrackingApp/PackageListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PackageListViewController.swift
+//  DeliveryTrackingApp
+//
+//  Created by Dav Sun on 3/3/17.
+//  Copyright © 2017 Download Horizons. All rights reserved.
+//
+
+import UIKit
+
+class PackageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
